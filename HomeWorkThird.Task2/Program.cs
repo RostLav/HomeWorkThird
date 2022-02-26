6463e4a34e028b719ffd67429a53ea783d6fa5f3
Rostislav <lavrenovrostislav@gmail.com>
2022-02-26 16:37:18 +0300

    Finishid Task 2

--- HomeWorkThird.Task2/Program.cs
+++ HomeWorkThird.Task2/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Пользователь вводит 1 число (A). Вывести все числа от 1 до 1000, которые делятся на A.
+Console.WriteLine("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+for(int i = numberA; i<=1000; i += numberA)
+{
+    Console.WriteLine(i);
+}
--- HomeWorkThird/Program.cs
+++ HomeWorkThird/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Пользователь вводит 2 числа (A и B). Возвести число A в степень B.
+Console.WriteLine("Введите число А:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+for()
