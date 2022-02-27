bea42ce85adbd92707b761bb0d27af6b17fbae2f
Rostislav <lavrenovrostislav@gmail.com>
2022-02-27 14:02:47 +0300

    Finished Task1

---
+++ HomeWorkThird.Task3/Program.cs
@@ -0,0 +1,8 @@
+﻿// Пользователь вводит 1 число (A). Найдите количество положительных целых чисел, квадрат которых меньше A.
+Console.WriteLine("Введите число А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+while (true)
+{
+
+}
+
--- HomeWorkThird/Program.cs
+++ HomeWorkThird/Program.cs
@@ -1,6 +1,17 @@
 ﻿//Пользователь вводит 2 числа (A и B). Возвести число A в степень B.
 Console.WriteLine("Введите число А:");
-int numberA = Convert.ToInt32(Console.ReadLine());
+double numberA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число В:");
-int numberB = Convert.ToInt32(Console.ReadLine());
-for()
+double numberB = Convert.ToInt32(Console.ReadLine());
+double answer = 1;
+for(int i = 0; (i<numberB&&numberB>0)||(i>numberB&&numberB<0);)
+    if(numberB<0){
+        answer /= numberA;
+        i--;
+    }
+    else
+    {
+        answer *= numberA;
+        i++;
+    }
+Console.Write(answer);
