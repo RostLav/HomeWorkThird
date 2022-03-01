0c514dafe5679b196c2ac505f307bcb8c9aeafd3
Rostislav <lavrenovrostislav@gmail.com>
2022-03-01 18:49:59 +0300

    Finishid Task6

--- HomeWorkThird.Task6/Program.cs
+++ HomeWorkThird.Task6/Program.cs
@@ -3,4 +3,21 @@
 // Первое и второе считаются равными 1.
 
 Console.WriteLine("Введите положительное число N");
-int numberN = 5;//Convert.ToInt32(Console.ReadLine());
+int numberN = Convert.ToInt32(Console.ReadLine());
+int firstNumber = 1;
+int secondNumber = 1;
+int tmp = 0;
+if(numberN <= 2)
+{
+    Console.WriteLine(1);
+}
+else
+{
+    for(int i=2; i<numberN; i++)
+    {
+        tmp = firstNumber + secondNumber;
+        firstNumber = secondNumber;
+        secondNumber = tmp;
+    }
+    Console.WriteLine(tmp);
+}
