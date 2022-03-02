c210dbe9e7582fe07bcf65cb80dbf54f55853fc8
Rostislav <lavrenovrostislav@gmail.com>
2022-03-02 10:57:34 +0300

    Completed Task9

--- HomeWorkThird.Task9/Program.cs
+++ HomeWorkThird.Task9/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Пользователь вводит 1 число. Найти количество нечетных цифр этого числа.
+Console.WriteLine("Введите число ");
+int numberN=Convert.ToInt32(Console.ReadLine());
+int answer = 0;
+
+while (numberN != 0)
+{
+    if ((numberN%10) % 2 == 1)
+    {
+        answer++;
+    }
+    numberN/=10;
+}
+Console.WriteLine($"Количество нечетных цифр цисла: {answer}");
